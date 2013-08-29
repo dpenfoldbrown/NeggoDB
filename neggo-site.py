bcf3d125dc19723644a048b1096f3ba81a7abbce
dpenfoldbrown <dpenfoldbrown@gmail.com>
2013-08-29 02:11:25 -0400

    Initial addition of neggo-site flask/python controller

---
+++ neggo-site.py
@@ -0,0 +1,17 @@
+"""
+NeggoDB (negative gene annotation database) Python+Flask-based web site definition
+
+@auth: dpb
+@date: 8/28/2013
+"""
+
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, _app_ctx_stack
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from db.neggoDB import *
+
+# Flask init
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
