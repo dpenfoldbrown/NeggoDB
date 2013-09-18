d5f5716f3a43d493647223a0c0ea90e86c290110
dpenfoldbrown <dpenfoldbrown@gmail.com>
2013-09-17 22:30:25 -0400

    Added local test dir to gitignore, added intial figure creation script

--- figures/validation_figures.py
+++ figures/validation_figures.py
@@ -4,4 +4,43 @@ Generate figures for validation data
 @date 9/17/2013
 """
 
+import matplotlib.pyplot as plt
 
+R_x = [6, 32, 64, 160, 319, 638, 957, 1275]
+R_y = [0,  0,  0,   0,   0,    2,  2,    4]
+
+N_x = [6, 32, 64, 160, 319, 638, 957, 1275]
+N_y = [1,  2,  3,   1,   0,    3,  1,    4]
+
+S_x = [6, 32, 64, 160, 319, 638, 957, 1275]
+S_y = [1,  1,  0,   0,   0,    1,  2,    3]
+
+rand_x = [6, 32, 64, 160, 319, 638, 957, 1275]
+rand_y = [4,  5,  5,   2,   6,   8,  4,     7]
+
+#TODO: Do not use scale data to pad charts
+
+xmax = max(R_x + N_x + S_x + rand_x)
+ymax = max(R_y + N_y + S_y + rand_y)
+xscale = int(xmax + (xmax * 0.01))
+yscale = int(ymax + (ymax * 0.4))
+scale_x = [0, xscale]
+scale_y = [0, yscale]
+
+fig = plt.figure()
+ax = fig.add_axes([0.1, 0.1, 0.75, 0.75])
+
+ax.set_title("Sample Accuracy: GO <GoID>, <GO Category>, <Organism>")
+ax.set_xlabel("Number of selected negative examples")
+ax.set_ylabel("Number of erroneous negative examples")
+
+ax.plot(scale_x, scale_y, ' ')
+ax.plot(R_x, R_y, 'm-', linewidth=2.0, label="Rocchio")
+ax.plot(N_x, N_y, 'r-', linewidth=2.0, label="NETL")
+ax.plot(S_x, S_y, 'y-', linewidth=2.0, label="SNOB")
+ax.plot(rand_x, rand_y, 'b-.', linewidth=2.0, label="Random")
+
+ax.legend(loc='upper left')
+
+plt.show()
+plt.close()
