b7aef48a4dd76618ff625809f5459676f8e7d52a
dpenfoldbrown <dpenfoldbrown@gmail.com>
2013-09-20 21:38:07 -0400

    Added additional routes and logging to nogo site py, and updated existing html files

--- nogo.py
+++ nogo.py
@@ -13,16 +13,46 @@ from db.nogoDB import *
 # Flask init
 app = Flask(__name__)
 
+# DB init via flask's sqlAlchemy extension
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dpb:dpb_nyu@handbanana.bio.nyu.edu:3306/noGO'
+db = SQLAlchemy(app)
 
 # Routes
-
 @app.route('/')
 def index():
     """Home page"""
     return render_template('index.html')
 
+@app.route('/downloads')
+def downloads():
+    """Static downloads page"""
+    return render_template('downloads.html')
+
+@app.route('/sources')
+def sources():
+    """Sources page"""
+    return render_template('sources.html')
+
+@app.route('/about')
+def about():
+    """About page"""
+    return render_template('about.html')
 
 if __name__ == "__main__":
     #WARNING: Do not run in debug mode publicly (allows for code execution)
     app.debug = True
+
+    # Logging setup
+    if not app.debug:
+        import logging
+        from logging.handlers import RotatingFileHandler
+        log_file = "nogo.web.log"
+        log_bytes = 10 * 1024 * 1024
+
+        file_handler = RotatingFileHandler(log_file, mode='a', maxBytes=log_bytes, backupCount=0)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s ' '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.DEBUG)
+        app.logger.addHandler(file_handler)
+
+    # app.run(host='0.0.0.0')
     app.run()
