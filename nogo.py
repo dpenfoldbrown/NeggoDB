1aa3755ac7d94cbb4223c1f3df9e9431391d6dd4
dpenfoldbrown <dpenfoldbrown@gmail.com>
2013-10-31 21:06:22 -0400

    Re-styled nogo.py, cleaned up code

--- nogo.py
+++ nogo.py
@@ -1,10 +1,11 @@
 """
-NeggoDB (negative gene annotation database) Python+Flask-based web site definition
+NeggoDB (negative gene annotation database) Python+Flask-based web site
 
 @auth: dpb
 @date: 8/28/2013
 """
 
+import os
 from datetime import datetime
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, send_file
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -22,7 +23,7 @@ db = SQLAlchemy(app)
 File_Location = "./static/files/generated/"
 NoGO_Version = 1
 Algorithm_ID_Dict = {
-    "Rocchio": 1, 
+    "Rocchio": 1,   
     "SNOB": 2,
     "NETL": 3
 }
@@ -38,10 +39,10 @@ Organism_DBC_Dict = {
 # Utility functions
 def generate_nogo_file(organism, branch, term, rocchio, netl, snob):
     """
-    Queries NoGO DB for negative example entries for given org, branch, and term (per alg). Writes 
-    neg examples to file, one row per alg. File also contains header info (on params).
-    Returns a tuple of (message, Filename URI). If creating the file fails for any reason, return is
-    ("error message", None). If successfull, returns path of file generated.
+    Queries NoGO DB for negative example entries for given org, branch, and term (per alg). Writes
+    neg examples to file, one row per alg. File also contains header info.
+    Returns a tuple of (message, Filename URI). If creating the file fails for any reason, return 
+    is ("error message", None). If successfull, returns ("success message", FilePath).
     """
 
     # Set up variables for DB query
@@ -56,46 +57,53 @@ def generate_nogo_file(organism, branch, term, rocchio, netl, snob):
     snob_examples = None
     if rocchio:
         rocchio_examples = db.session.query(organism_dbc).filter_by(
-                go_category=branch, 
-                go_id=term, 
+                go_id=term,
+                go_category=branch,
+                version_id=NoGO_Version,
                 algorithm_id=Algorithm_ID_Dict["Rocchio"])
     if netl:
         netl_examples = db.session.query(organism_dbc).filter_by(
-                go_category=branch, 
                 go_id=term,
+                go_category=branch,
+                version_id=NoGO_Version,
                 algorithm_id=Algorithm_ID_Dict["NETL"])
     if snob:
         snob_examples = db.session.query(organism_dbc).filter_by(
-                go_category=branch, 
                 go_id=term,
+                go_category=branch, 
+                version_id=NoGO_Version,
                 algorithm_id=Algorithm_ID_Dict["SNOB"])
 
     # Check query results. If all empty, return None with error message "No results"
-    # TODO: May have to/want to do this with counts, not existence (if no results, what is query return value with no all() or first()?)
+    #TODO: May have to/want to do this with counts, not existence (if no results, what is query 
+    #TODO: return value with no all() or first()?)
     if rocchio_examples == None and netl_examples == None and snob_examples == None:
         return ("No results returned for given values (check GO Term!)", None)
 
     # Create filename from parameters
-    # TODO: Create secure temporary filename for results in the File_Location dir (via python module secure file of whatever)
-    outfile = os.path.join(File_Location, "{0}-{1}-{2}-{3}.txt".format(organism, branch, term, datetime.now()))
-    with open(outfile) as outhandle:
+    #TODO: Create secure temporary filename for results in the File_Location dir (via python module
+    #TODO: secure file of whatever)
+    outfile = os.path.join(File_Location, "{0}-{1}-{2}-{3}.txt".format(
+        organism, branch, term, datetime.now()))
 
-        # Print file header information
-        outhandle.write("--------->NoGO Negative gene examples: {0},{1},{2}\n".format(organism, term, branch))
-        outhandle.write("<Algorithm>:    <1stMostNegGene>    <2ndMostNegGene>    <3rdMostNegGene>...\n")
+    with open(outfile) as handle:
+
+        handle.write("** NoGO negative gene examples: {0}, {1}, {2}\n".format(organism, term, 
+            branch))
+        handle.write("** <Algorithm>:  <1stMostNegGene>  <2ndMostNegGene>  <3rdMostNegGene>...\n")
 
         if rocchio_examples:
-            _write_genes_to_file("Rocchio", rocchio_examples, outhandle)
+            _write_genes_to_file("Rocchio", rocchio_examples, handle)
         if netl:
-            _write_genes_to_file("NETL", netl_examples, outhandle)
+            _write_genes_to_file("NETL", netl_examples, handle)
         if snob:
-            _write_genes_to_file("SNOB", snob_examples, outhandle)
+            _write_genes_to_file("SNOB", snob_examples, handle)
 
     return ("Successfuly wrote outfile {0}".format(outfile), outfile)
 
 
 def _write_genes_to_file(algorithm_name, results_cursor, file_handle):
-    """Writes gene_symbol from every item in results cursor to a tab-delimed line in file_handle"""
+    """Write gene_symbol from every item in results cursor to a tab-delimed line in file_handle"""
     file_handle.write("{0}:".format(algorithm_name))
     for neg_eg in results_cursor.all():
         file_handle.write("\t{0}".format(neg_eg.gene_symbol))
@@ -106,31 +114,36 @@ def _write_genes_to_file(algorithm_name, results_cursor, file_handle):
 @app.route('/data', methods=['POST'])
 def data_request():
     """
-    Method to handle requests for data. Parameters in the 'request' object (parse and check first).
-    Should have: Organism, Go Category, Go Term [optional], Algorithm [1+]
+    Method to handle requests for data. Parameters in the 'request' object (parse and check first)
+    Should have: Organism, Go Category, Go Term, and Algorithm [1+]
     """
     # Check request
     if request.method != "POST":
-        return render_template('bad_request.html', request=request, message="Server Error: Request not POST")
+        return render_template('bad_request.html', request=request, 
+            message="Server Error: Request not POST")
     
     # Get values from request
     if "Organism" not in request.form:
-        return render_template('bad_request.html', request=request, message="Must select a valid Organism")
+        return render_template('bad_request.html', request=request, 
+            message="Must select a valid Organism")
     organism = request.form["Organism"]
     
     if "Branch" not in request.form:
-        return render_template('bad_request.html', request=request, message="Must select a valid GO Branch")
+        return render_template('bad_request.html', request=request, 
+            message="Must select a valid GO Branch")
     branch = request.form["Branch"]
         
     if "Term" not in request.form:
-        return render_template('bad_request.html', request=request, message="Must specify a valid GO Term")
+        return render_template('bad_request.html', request=request, 
+            message="Must specify a valid GO Term")
     term = request.form["Term"]
 
     rocchio = True if "Rocchio" in request.form else False
     netl = True if "NETL" in request.form else False
     snob = True if "SNOB" in request.form else False
     if not (rocchio or netl or snob):
-        return render_template('bad_request.html', request=request, message="Must select at least one Algorithm")
+        return render_template('bad_request.html', request=request, 
+            message="Must select at least one Algorithm")
 
     # Call file-generator with request values
     (file_message, results_file) = generate_nogo_file(organism, branch, term, rocchio, netl, snob)
@@ -140,10 +153,10 @@ def data_request():
                 request=request, 
                 message="Generating Negative Example file failed: {0}".format(file_message))
 
-    # Send file to user. TODO: Look at send_file docs, see options
     try:
+        #TODO: Re-render the home template, or render a "Download will begin shortly (with loading
+        #TODO: icon) page?"
         send_file(results_file, mimetype="application/text", as_attachment=True)
-        #TODO: Re-render the home template, or render a "Download will begin shortly (with loading icon) page?"
     except Exception as e:
         print "Exception when attempting send_file: {0}".format(e)
         return render_template('bad_request.html', request=request, message="Failed to send file")
@@ -179,7 +192,8 @@ if __name__ == "__main__":
         log_bytes = 10 * 1024 * 1024
 
         file_handler = RotatingFileHandler(log_file, mode='a', maxBytes=log_bytes, backupCount=0)
-        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s ' '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s ' '[in %(pathname)s:%(lineno)d]'))
         file_handler.setLevel(logging.DEBUG)
         app.logger.addHandler(file_handler)
 
